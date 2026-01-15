76a4f0a143e4b3d50f808133c9d95da0ceb66c41
lumixuser002 <51259213+playanose@users.noreply.github.com>
2026-01-15 10:35:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('p-1').then(c => c.addAll(['./', 'index.html', 'manifest.json'])));
+  e.waitUntil(caches.open('gcam-v1').then(c => c.addAll(['index.html', 'manifest.json'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
